0d47cf92a7c2c96f4cfff1b1c5075b3ac9c0a68b
Adam Zielinski <github@maosmurf.com>
2024-09-02 13:39:39 +0200

    mock.method

--- src/yolo.spec.js
+++ src/yolo.spec.js
@@ -1,10 +1,23 @@
 const assert = require('node:assert');
 const { describe, it } = require('node:test');
 const { doStuff } = require('./yolo');
+const { mock } = require("node:test");
+const friends = require('./friends');
+
+mock.method(friends, 'status', async () => {
+    return Promise.resolve('fake')
+});
 
 describe('yolo', () => {
-    it('works', async () => {
+    it('works with top-level fake', async () => {
         const res = await doStuff();
-        assert.equal(res, 'it\'s complicated');
+        assert.equal(res, 'fake');
+    });
+    it('works with nested fake', async () => {
+        mock.method(friends, 'status', async () => {
+            return Promise.resolve('nested fake')
+        });
+        const res = await doStuff();
+        assert.equal(res, 'nested fake');
     });
 });
